e575ba975661ae6f22784e932d2a8446a15a9b0a
Fred Offei <112915741+danexpress@users.noreply.github.com>
2023-12-05 17:12:55 -0500

    function based test

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -6,3 +6,18 @@ import source.my_functions as my_functions
 def test_add():
     result = my_functions.add(number_one=1, number_two=4)
     assert result == 5
+
+
+def test_add_strings():
+    result = my_functions.add(number_one="i like ", number_two="baddies")
+    assert result == "i like baddies"
+
+
+def test_divide():
+    result = my_functions.divide(number_one=10, number_two=5)
+    assert result == 2
+
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        my_functions.divide(number_one=10, number_two=0)
