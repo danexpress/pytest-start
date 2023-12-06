e3e7172fd011ef6fbbda13282f6e0ffc502ab394
Fred Offei <112915741+danexpress@users.noreply.github.com>
2023-12-05 21:15:32 -0500

    mark and padrametrize

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,5 +1,5 @@
 import pytest
-
+import time
 import source.my_functions as my_functions
 
 
@@ -21,3 +21,20 @@ def test_divide():
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
         my_functions.divide(number_one=10, number_two=0)
+
+
+@pytest.mark.slow
+def test_very_slow():
+    time.sleep(5)
+    result = my_functions.divide(number_one=10, number_two=5)
+    assert result == 2
+
+
+@pytest.mark.skip(reason="This feature is broken")
+def test_add():
+    assert my_functions.add(number_one=2, number_two=3) == 7
+
+
+@pytest.mark.xfail(reason="We know we cannot divide by zero")
+def test_divide_zero_broken():
+    my_functions.divide(number_one=4, number_two=0)
